63d19ec406eedd6c9a47f3b160ee15979b3b3406
Rob Patro <rob.patro@gmail.com>
2024-07-17 14:58:23 -0400

    rearrange as proper library with prior main as an example

--- examples/compare_with_python.rs
+++ examples/compare_with_python.rs
@@ -1,15 +1,70 @@
-mod kde;
-mod kde_function;
-use crate::kde_function::kde_computation_py;
-
 use ordered_float::OrderedFloat;
 use rand::distributions::{Distribution, Uniform};
 use rand::prelude::*;
 use std::time::Instant;
 
+use kders::kde::GridDimensions;
+use ndarray::Array2;
+use numpy::IntoPyArray;
+use numpy::{PyArray1, PyArray2};
+use pyo3::prelude::*;
+use pyo3::prepare_freethreaded_python;
+
+use std::io;
+
+pub fn kde_computation_py(
+    data: &[f64],
+    weight: &[f64],
+    bin_width: f64, //store: &mut InMemoryAlignmentStore,
+    bandwidth: Option<f64>,
+    grid_dim: Option<GridDimensions>,
+) -> io::Result<Vec<f64>> {
+    // Initialize the Python interpreter
+    prepare_freethreaded_python();
+
+    Python::with_gil(|py| {
+        // Import the Python module and function
+        let module = PyModule::from_code_bound(
+            py,
+            include_str!("2D_kde_function.py"),
+            "2D_kde_function",
+            "2D_kde_function.py",
+        )?;
+        let function = module.getattr("calculate_kde")?;
+
+        // Example 2D data
+        let data = Array2::from_shape_vec(((data.len() / 2), 2), data.to_vec()).unwrap();
+
+        // Convert data to PyArray2
+        let data_py: pyo3::Bound<'_, PyArray2<f64>> = data.view().to_owned().into_pyarray_bound(py);
+
+        // Dummy weights for demonstration purposes
+        let weights = Array2::from_shape_vec((weight.len(), 1), weight.to_vec()).unwrap();
+
+        // Convert weights to PyArray2
+        let weights_py: pyo3::Bound<'_, PyArray2<f64>> =
+            weights.view().to_owned().into_pyarray_bound(py);
+
+        let (max_x, max_y) = match grid_dim {
+            Some(GridDimensions { width, height }) => (Some(width), Some(height)),
+            None => (None, None),
+        };
+        // Call the Python function
+        println!("it is before python function");
+        let result: Py<PyArray1<f64>> = function
+            .call1((data_py, weights_py, bin_width, bandwidth, max_x, max_y))?
+            .extract()?;
+        //println!("result length: {:?}", result.as_ref().len());
+        let r: Vec<f64> = result.extract(py)?;
+        Ok(r)
+    })
+}
 fn main() -> anyhow::Result<()> {
     let mut rng = rand::thread_rng();
-    let die = Uniform::from(10.0..500.0);
+
+    let max_val = 3000;
+
+    let die = Uniform::from(10.0..(max_val as f64));
     let mut data = Vec::<f64>::with_capacity(200);
     let mut weights = Vec::<f64>::with_capacity(100);
     let mut weight_sum = 0_f64;
@@ -17,7 +72,7 @@ fn main() -> anyhow::Result<()> {
     let kernel_bandwidth = 5_f64;
     let bin_width = 10_usize;
 
-    for _i in 0..2000 {
+    for _i in 0..5000 {
         let x: f64 = die.sample(&mut rng);
         let y: f64 = die.sample(&mut rng);
         data.push(x.round());
@@ -27,9 +82,9 @@ fn main() -> anyhow::Result<()> {
         weight_sum += weights.last().unwrap();
     }
 
-    let gd = crate::kde::GridDimensions {
-        width: 500,
-        height: 500,
+    let gd = kders::kde::GridDimensions {
+        width: max_val as usize,
+        height: max_val as usize,
     };
 
     //python kde fucntion
@@ -60,7 +115,7 @@ fn main() -> anyhow::Result<()> {
     */
 
     let rust_start = Instant::now();
-    let mut grid = crate::kde::KDEGrid::new(gd, bin_width, Some(kernel_bandwidth));
+    let mut grid = kders::kde::KDEGrid::new(gd, bin_width, Some(kernel_bandwidth));
     for (chunk, w) in data.chunks(2).zip(weights.iter()) {
         grid.add_observation(chunk[0] as usize, chunk[1] as usize, *w);
     }
