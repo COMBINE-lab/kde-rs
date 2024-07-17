b7a851ff55e85e27df392da29d44219485fc7425
Rob Patro <rob.patro@gmail.com>
2024-07-17 17:01:06 -0400

    Start adding basic documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,6 @@
+//! The `kde-rs` crate provides an implementation of a 2D kernel density
+//! estimator for a restricted set of situations. It focuses on being
+//! easy to use, and on being efficient in the scenario for which it
+//! has been designed.
+
 pub mod kde;
